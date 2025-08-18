868bee1110f42eae17cc5de3d6077e6c42d3c0d8
vickycode88 <vickypd2003@gmail.com>
2025-08-18 19:46:15 +0530

    new react file

---
+++ myapp/src/App.js
@@ -0,0 +1,11 @@
+import { Component } from "react"
+
+export function Avenger() {
+    return <h1>Avenger Assemble</h1>
+}
+
+export class AvengerClass extends Component {
+    render() {
+        return <h1>Avenger Assemble by class</h1>
+    }
+}
---
+++ myapp/src/index.js
@@ -0,0 +1,15 @@
+import { Avenger } from "./App";
+import React from "react";
+import ReactDom from "react-dom/client"
+import { AvengerClass } from "./App";
+
+const container = document.getElementById('root')
+const root = ReactDom.createRoot(container)
+// root.render(<Avenger />)
+// root.render(<AvengerClass />)
+root.render(<>
+
+    <Avenger />
+    <AvengerClass />
+</>)
+
