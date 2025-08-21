135ec55711cc2236d4849dc93cd5f48a0b96da3b
vickycode88 <vickypd2003@gmail.com>
2025-08-21 18:37:13 +0530

    react_2_prac

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,25 @@
-import { Component } from "react"
+// import { Component } from "react"
 
-export function Avenger() {
-    return <h1>Avenger Assemble</h1>
-}
+// export function Avenger() {
+//     return <h1>Avenger Assemble</h1>
+// }
 
-export class AvengerClass extends Component {
-    render() {
-        return <h1>Avenger Assemble by class</h1>
-    }
+// export class AvengerClass extends Component {
+//     render() {
+//         return <h1>Avenger Assemble by class</h1>
+//     }
+// }
+
+
+
+import { Header, Avenger, Footer, AvengerClass} from "./Shield";
+
+export function ShieldPass() {
+    return <>
+        <Header />
+        <Avenger fname='steve' lname='Roger' />
+        <Avenger fname='Tony' lname='stark' />
+        <AvengerClass fname='Bruce' lname='Banner' />
+        <Footer />
+    </>
 }
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,15 +1,26 @@
-import { Avenger } from "./App";
-import React from "react";
-import ReactDom from "react-dom/client"
-import { AvengerClass } from "./App";
+// import { Avenger } from "./App";
+// import React from "react";
+// import ReactDom from "react-dom/client"
+// import { AvengerClass } from "./App";
+
+// const container = document.getElementById('root')
+// const root = ReactDom.createRoot(container)
+// // root.render(<Avenger />)
+// // root.render(<AvengerClass />)
+// root.render(<>
+
+//     <Avenger />
+//     <AvengerClass />
+// </>)
+
+import { ShieldPass } from "./App";
+
+import React from 'react';
+import ReactDom from 'react-dom/client'
 
 const container = document.getElementById('root')
 const root = ReactDom.createRoot(container)
-// root.render(<Avenger />)
-// root.render(<AvengerClass />)
-root.render(<>
+root.render(<ShieldPass/>)
 
-    <Avenger />
-    <AvengerClass />
-</>)
+
 
